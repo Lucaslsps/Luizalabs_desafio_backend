98313766a2ea990b6cdfecaba060c0384001b43a
Lucas Peixoto <lucas.peixoto@venhapranuvem.com.br>
2022-06-11 15:11:52 -0300

    Swagger

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import gameRoutes from '@modules/game/infra/http/routes/game.routes';
 import HealthRouter from '@modules/health/infra/http/routes/health.routes';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDoc from './swagger.json';
 
 const routes = Router();
 
 routes.use('/v1/game', gameRoutes);
 routes.use('/v1/health', HealthRouter);
+routes.use('/v1/documentation', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 export default routes;
